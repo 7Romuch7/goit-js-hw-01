1f7a7fdadfa3bedc7be9180602d34e8220cf5934
7Romuch7 <64313145+7Romuch7@users.noreply.github.com>
2020-08-13 00:17:33 +0300

    JavaScript

---
+++ js/task-03.js
@@ -0,0 +1,18 @@
+if (userPassword === ADMIN_PASSWORD) {
+    message = ACCESS_IS_ALLOWED;} else if (userPassword === null) {
+    message = CANCELED_BY_USER;
+  } else {
+    message = ACCESS_DENIED;
+  }
+
+//если userPassword равно 'jqueryismyjam' 
+// то значение message будет равно
+// 'Добро пожаловать!'
+
+//если userPassword равно  null 
+// то значение message будет равно  
+// 'Отменено пользователем!'
+
+//если userPassword равно '123' 
+// то значение message будет равно
+// 'Доступ запрещен, неверный пароль!'
